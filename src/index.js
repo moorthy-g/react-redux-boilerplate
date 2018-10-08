d8cd1763263d611ec34d495c9eadf21bb2c2ca8d
Moorthy <moorthy.g@krds.fr>
2018-10-08 15:24:37 +0530

    Create 'src/styles' directory for common styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from 'screens/app';
+import './styles/common.less';
 
 if (module.hot) {
   module.hot.accept();
